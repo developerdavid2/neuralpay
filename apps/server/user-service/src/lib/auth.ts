d61952431121e572c6731d8d48add6897279e07b
Jacobs David <jacobsdavid.dr@gmail.com>
2026-07-19 13:58:17 +0100

    fiz base url for auth

--- apps/server/user-service/src/lib/auth.ts
+++ apps/server/user-service/src/lib/auth.ts
@@ -1,16 +1,14 @@
 import { createAuth } from "@neuralpay/auth";
 import { userServiceEnv } from "@neuralpay/env/user-service";
 
-export const auth = () => {
-  createAuth({
-    secret: userServiceEnv.BETTER_AUTH_SECRET,
-    baseURL: userServiceEnv.BETTER_AUTH_URL,
-    google:
-      userServiceEnv.GOOGLE_CLIENT_ID && userServiceEnv.GOOGLE_CLIENT_SECRET
-        ? {
-            clientId: userServiceEnv.GOOGLE_CLIENT_ID,
-            clientSecret: userServiceEnv.GOOGLE_CLIENT_SECRET,
-          }
-        : undefined,
-  });
-};
+export const auth = createAuth({
+  secret: userServiceEnv.BETTER_AUTH_SECRET,
+  baseURL: userServiceEnv.BETTER_AUTH_URL,
+  google:
+    userServiceEnv.GOOGLE_CLIENT_ID && userServiceEnv.GOOGLE_CLIENT_SECRET
+      ? {
+          clientId: userServiceEnv.GOOGLE_CLIENT_ID,
+          clientSecret: userServiceEnv.GOOGLE_CLIENT_SECRET,
+        }
+      : undefined,
+});
