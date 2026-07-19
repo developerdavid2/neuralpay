3ebdd9e611722819f36116eb9ac942bf035fec95
Jacobs David <jacobsdavid.dr@gmail.com>
2026-07-19 15:35:33 +0100

    remove next cofig rewrite and allow vercel json rewrite

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,24 +1,9 @@
-import { webEnv } from "@neuralpay/env/web";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
   reactCompiler: true,
   output: "standalone",
-
-  async rewrites() {
-    const serverUrl = webEnv.SERVER_URL ?? webEnv.NEXT_PUBLIC_SERVER_URL;
-    return [
-      {
-        source: "/api/auth/:path*",
-        destination: `${serverUrl}/v1/auth/:path*`,
-      },
-      {
-        source: "/api/trpc/:path*",
-        destination: `${serverUrl}/v1/trpc/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
