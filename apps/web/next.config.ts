c0be899e2971eb158c7df66318833f077832cc02
Jacobs David <jacobsdavid.dr@gmail.com>
2026-07-19 14:01:53 +0100

    remove rewrite from next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -5,20 +5,20 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
 
-  async rewrites() {
-    const serverUrl =
-      process.env.SERVER_URL ?? process.env.NEXT_PUBLIC_SERVER_URL;
-    return [
-      {
-        source: "/api/auth/:path*",
-        destination: `${serverUrl}/v1/auth/:path*`,
-      },
-      {
-        source: "/api/trpc/:path*",
-        destination: `${serverUrl}/v1/trpc/:path*`,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   const serverUrl =
+  //     process.env.SERVER_URL ?? process.env.NEXT_PUBLIC_SERVER_URL;
+  //   return [
+  //     {
+  //       source: "/api/auth/:path*",
+  //       destination: `${serverUrl}/v1/auth/:path*`,
+  //     },
+  //     {
+  //       source: "/api/trpc/:path*",
+  //       destination: `${serverUrl}/v1/trpc/:path*`,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
