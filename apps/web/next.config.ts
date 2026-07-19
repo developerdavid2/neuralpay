0752bd6c9290d16bbbbcbf71d55508f26044fed7
Jacobs David <jacobsdavid.dr@gmail.com>
2026-07-19 14:04:23 +0100

    use env variable to set rewrite

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,3 +1,4 @@
+import { webEnv } from "@neuralpay/env/web";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -5,20 +6,19 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
 
-  // async rewrites() {
-  //   const serverUrl =
-  //     process.env.SERVER_URL ?? process.env.NEXT_PUBLIC_SERVER_URL;
-  //   return [
-  //     {
-  //       source: "/api/auth/:path*",
-  //       destination: `${serverUrl}/v1/auth/:path*`,
-  //     },
-  //     {
-  //       source: "/api/trpc/:path*",
-  //       destination: `${serverUrl}/v1/trpc/:path*`,
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    const serverUrl = webEnv.SERVER_URL ?? webEnv.NEXT_PUBLIC_SERVER_URL;
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: `${serverUrl}/v1/auth/:path*`,
+      },
+      {
+        source: "/api/trpc/:path*",
+        destination: `${serverUrl}/v1/trpc/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
