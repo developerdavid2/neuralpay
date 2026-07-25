789a071948a1b193ef24706dabed73eb3a7d8811
Jacobs David <jacobsdavid.dr@gmail.com>
2026-07-25 14:57:16 +0100

    modify response headers instead of request headers in nextjs middleware

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -1,13 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers);
-  requestHeaders.set("x-pathname", request.nextUrl.pathname);
-  return NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  });
+  const response = NextResponse.next();
+  response.headers.set("x-pathname", request.nextUrl.pathname);
+  return response;
 }
 
 export const config = {
